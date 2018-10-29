4ad141fb7cfd05e1def57edb9d0c574785ba53a2
Sönke Behrendt <soenke.behrendt@gmail.com>
2018-10-29 18:09:37 +0100

    Integrate PhraseApp android-sdk

---
+++ app/src/main/java/com/phraseapp/example/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.phraseapp.example;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.phraseapp.android.sdk.PhraseApp;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(PhraseApp.wrap(newBase));
+    }
+}
---
+++ app/src/main/java/com/phraseapp/example/MainApplication.java
@@ -0,0 +1,13 @@
+package com.phraseapp.example;
+
+import android.app.Application;
+
+import com.phraseapp.android.sdk.PhraseApp;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PhraseApp.init(this, "", "");
+    }
+}
