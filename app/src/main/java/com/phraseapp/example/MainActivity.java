5a1c94f15792787d3abaa0631c9d7bda2631e42c
Sönke Behrendt <soenke.behrendt@gmail.com>
2019-02-05 13:06:33 +0100

    Add example string to main activity

--- app/src/main/java/com/phraseapp/example/MainActivity.java
+++ app/src/main/java/com/phraseapp/example/MainActivity.java
@@ -3,7 +3,7 @@ package com.phraseapp.example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
