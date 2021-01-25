fbaf9abf9223c4d8ecdf8c1ca44ff7b52b1b87c6
Sönke Behrendt <soenke.behrendt@gmail.com>
2021-01-25 10:20:51 +0100

    Update to SDK version 3.0.0

--- app/src/main/java/com/phrase/example/MainApplication.java
+++ app/src/main/java/com/phrase/example/MainApplication.java
@@ -1,6 +1,7 @@
 package com.phrase.example;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.phrase.android.sdk.Phrase;
 
@@ -8,7 +9,13 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         Phrase.setup(this, "", "");
         Phrase.updateTranslations();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(Phrase.wrapApplicationContext(newBase));
     }
 }
